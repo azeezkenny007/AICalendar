d499282be21ead30bd10ec0e7868d26a6be8a43c
azeezkenny007 <azeezokhamena@gmail.com>
2025-11-28 13:18:46 +0100

    Add AggregateRoot, DomainEvent, and IDomainEvent abstractions for event sourcing

---
+++ AICalendar.CORE/Abstractions/DomainEvent.cs
@@ -0,0 +1,10 @@
+using MediatR;
+
+namespace AICalendar.CORE.Abstractions;
+
+public abstract record DomainEvent : IDomainEvent, INotification
+{
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    public string EventType => GetType().Name;
+}
---
+++ AICalendar.CORE/Abstractions/IDomainEvent.cs
@@ -0,0 +1,8 @@
+namespace AICalendar.CORE.Abstractions;
+
+public interface IDomainEvent
+{
+    Guid EventId { get; }
+    DateTime OccurredOn { get; }
+    string EventType { get; }
+}

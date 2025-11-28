392a974a67f825009040b0633ce15bfec3e47f9d
azeezkenny007 <azeezokhamena@gmail.com>
2025-11-28 16:16:01 +0100

    feat: add TransactionId value object with creation methods and parameterless constructor

---
+++ src/AICalendar.Domain/ValueObjects/TransactionId.cs
@@ -0,0 +1,19 @@
+namespace AICalendar.Domain.ValueObjects;
+
+public record TransactionId
+{
+    public Guid Value { get; init; }
+
+    // EF Core needs a parameterless constructor
+    private TransactionId() { }
+
+    private TransactionId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static TransactionId Create() => new(Guid.NewGuid());
+    public static TransactionId Create(Guid value) => new(value);
+
+    public override string ToString() => Value.ToString();
+}

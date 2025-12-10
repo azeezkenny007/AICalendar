9c6d952ce377b616d2d40039f389d5fcbada4654
azeezkenny007 <azeezokhamena@gmail.com>
2025-12-10 14:05:58 +0100

    `Update SQL Server connection options in Program.cs`

--- src/AICalendar.API/Program.cs
+++ src/AICalendar.API/Program.cs
@@ -112,7 +112,17 @@ builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =
 {
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-    options.UseSqlServer(connectionString);
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        // Enable retry on transient failures (Azure SQL recommended)
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null);
+
+        // Set command timeout to 60 seconds
+        sqlOptions.CommandTimeout(60);
+    });
 
     // Set global query tracking behavior to NoTracking
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);

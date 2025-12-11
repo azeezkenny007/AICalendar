5a63b7446794b66c76c7a8d1039f49eb1f6db81e
azeezkenny007 <azeezokhamena@gmail.com>
2025-12-11 15:08:18 +0100

    n

---
+++ src/AICalendar.Infrastructure/Migrations/20251211133858_kat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AICalendar.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class kat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 13, 38, 56, 229, DateTimeKind.Utc).AddTicks(3135));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 13, 38, 56, 229, DateTimeKind.Utc).AddTicks(3135));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 13, 38, 56, 229, DateTimeKind.Utc).AddTicks(3135));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 13, 38, 56, 229, DateTimeKind.Utc).AddTicks(3135));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 8, 46, 56, 119, DateTimeKind.Utc).AddTicks(683));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 8, 46, 56, 119, DateTimeKind.Utc).AddTicks(683));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 8, 46, 56, 119, DateTimeKind.Utc).AddTicks(683));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 8, 11, 8, 46, 56, 119, DateTimeKind.Utc).AddTicks(683));
+        }
+    }
+}
